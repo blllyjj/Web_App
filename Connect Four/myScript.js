158234d81c499a2c69c65ee6e87c7251cb0de735
Billy J <50288768+blllyjj@users.noreply.github.com>
2020-05-03 21:38:56 -0400

    Add files via upload

---
+++ Connect Four/myScript.js
@@ -0,0 +1,6 @@
+//function to find the color of a button
+//function to change the color of a button; by supplying the col & row index
+//function to check the last button aviable row(check bottom)(aka the last aviable grey button)
+//function to check for the same same input of the same color(Connect Four)
+//Win Check: Horizontal, Vertical, Diagonal
+//Check if the game has ended: No Body Won, Somebody Won
