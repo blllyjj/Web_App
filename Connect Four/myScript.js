040ab4275f6e36fd133eea429637812b880236a6
Billy J <50288768+blllyjj@users.noreply.github.com>
2020-05-04 21:51:32 -0400

    Update myScript.js

--- Connect Four/myScript.js
+++ Connect Four/myScript.js
@@ -4,3 +4,149 @@
 //function to check for the same same input of the same color(Connect Four)
 //Win Check: Horizontal, Vertical, Diagonal
 //Check if the game has ended: No Body Won, Somebody Won
+
+//start by promting, asking players for names and infomation
+//basic information
+// We need to use jQuery for the following:
+
+var player1 = prompt("一號玩家 輸入你的名字");
+var player1Color = 'rgb(86, 151, 255)';
+
+var player2 = prompt("二號玩家 輸入你的名字");
+var player2Color = 'rgb(237, 45, 73)';
+
+var game_on = true;
+var table = $('table tr');
+
+
+function reportWin(rowNum,colNum) {
+  console.log("You won starting at this row,col");
+  console.log(rowNum);
+  console.log(colNum);
+}
+// Change the color of a button
+function changeColor(rowIndex,colIndex,color) {
+  table.eq(rowIndex).find('td').eq(colIndex).find('button').css('background-color',color);
+}
+
+// Report Back to current color of a button
+function returnColor(rowIndex,colIndex) {
+  return table.eq(rowIndex).find('td').eq(colIndex).find('button').css('background-color');
+}
+
+// Take in column index, returns the bottom row that is still gray
+function checkBottom(colIndex) {
+  var colorReport = returnColor(5,colIndex);
+  for (var row = 5; row > -1; row--) {
+    colorReport = returnColor(row,colIndex);
+    if (colorReport === 'rgb(128, 128, 128)') {
+      return row
+    }
+  }
+}
+
+// Check to see if 4 inputs are the same color
+function colorMatchCheck(one,two,three,four){
+  return (one===two && one===three && one===four && one !== 'rgb(128, 128, 128)' && one !== undefined);
+}
+
+// Check for Horizontal Wins
+function horizontalWinCheck() {
+  for (var row = 0; row < 6; row++) {
+    for (var col = 0; col < 7; col++) {
+      if (colorMatchCheck(returnColor(row,col), returnColor(row,col+1) ,returnColor(row,col+2), returnColor(row,col+3))) {
+        console.log('horiz');
+        reportWin(row,col);
+        return true;
+      }else {
+        continue;
+      }
+    }
+  }
+}
+
+// Check for Vertical Wins
+function verticalWinCheck() {
+  for (var col = 0; col < 7; col++) {
+    for (var row = 0; row < 3; row++) {
+      if (colorMatchCheck(returnColor(row,col), returnColor(row+1,col) ,returnColor(row+2,col), returnColor(row+3,col))) {
+        console.log('vertical');
+        reportWin(row,col);
+        return true;
+      }else {
+        continue;
+      }
+    }
+  }
+}
+
+// Check for Diagonal Wins
+function diagonalWinCheck() {
+  for (var col = 0; col < 7; col++) {
+    for (var row = 0; row < 6; row++) {
+      if (colorMatchCheck(returnColor(row,col), returnColor(row+1,col+1) ,returnColor(row+2,col+2), returnColor(row+3,col+3))) {
+        console.log('diag');
+        reportWin(row,col);
+        return true;
+      }else if (colorMatchCheck(returnColor(row,col), returnColor(row-1,col+1) ,returnColor(row-2,col+2), returnColor(row-3,col+3))) {
+        console.log('diag');
+        reportWin(row,col);
+        return true;
+      }else {
+        continue;
+      }
+    }
+  }
+}
+
+// Game End
+function gameEnd(winningPlayer) {
+  for (var col = 0; col < 7; col++) {
+    for (var row = 0; row < 7; row++) {
+      $('h3').fadeOut('fast');
+      $('h2').fadeOut('fast');
+      $('h1').text(winningPlayer+" 你贏了 刷新重新玩吧!").css("fontSize", "50px")
+    }
+  }
+}
+
+// Start with Player One
+var currentPlayer = 1;
+var currentName = player1;
+var currentColor = player1Color;
+
+// Start with Player One
+$('h3').text(player1+" 你先");
+
+$('.board button').on('click',function() {
+
+  // Recognize what column was chosen
+  var col = $(this).closest("td").index();
+
+  // Get back bottom available row to change
+  var bottomAvail = checkBottom(col);
+
+  // Drop the chip in that column at the bottomAvail Row
+  changeColor(bottomAvail,col,currentColor);
+
+  // Check for a win or a tie.
+  if (horizontalWinCheck() || verticalWinCheck() || diagonalWinCheck()) {
+    gameEnd(currentName);
+  }
+
+  // If no win or tie, continue to next player
+  currentPlayer = currentPlayer * -1 ;
+
+  // Re-Check who the current Player is.
+  if (currentPlayer === 1) {
+    currentName = player1;
+    $('h3').text(currentName+" 到你了! ");
+    currentColor = player1Color;
+  }else {
+    currentName = player2
+    $('h3').text(currentName+" 到你了! ");
+    currentColor = player2Color;
+  }
+
+})
+
